1d7d49071491ac0c27813ffe4e3eadfd24b865e2
Zahir Bobat <zfbobat@gmail.com>
2022-02-01 22:47:20 +0000

    wip:Added code for drawer navigator
    Not working

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+//Import my own files
+import {HomeScreen} from './Pages/Homescreen.js' ;
+import {SettingsScreen} from './Pages/Settings.js' ;
+import {ProfileScreen} from './Pages/Profile.js' ;
+
+function homeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen!</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={homeScreen} />
+          <Drawer.Screen name="Home" component={SettingsScreen} />
+          <Drawer.Screen name="Home" component={ProfileScreen} />
+        </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +40,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
